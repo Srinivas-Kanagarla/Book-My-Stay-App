858b3efe4263791fab2ee66e7ad42e998012bb87
Srinivas-Kanagarla <kangaralasrinivas2007@gmail.com>
2026-03-24 14:30:21 +0530

    UC1:Application Entry & Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,54 @@
+abstract class Room {
+  private String type;
+  private int beds;
+  private double price;
+
+  public Room(String type, int beds, double price) {
+    this.type = type;
+    this.beds = beds;
+    this.price = price;
+  }
+
+  public void displayDetails(int availability) {
+    System.out.println("Room Type: " + type);
+    System.out.println("Beds: " + beds);
+    System.out.println("Price: $" + price);
+    System.out.println("Available Units: " + availability);
+    System.out.println("---------------------------");
+  }
+}
+
+class SingleRoom extends Room {
+  public SingleRoom() {
+    super("Single Room", 1, 100.0);
+  }
+}
+
+class DoubleRoom extends Room {
+  public DoubleRoom() {
+    super("Double Room", 2, 180.0);
+  }
+}
+
+class SuiteRoom extends Room {
+  public SuiteRoom() {
+    super("Luxury Suite", 3, 350.0);
+  }
+}
 
 public class Main {
-
-
   public static void main(String[] args) {
+    Room single = new SingleRoom();
+    Room cpuDouble = new DoubleRoom();
+    Room suite = new SuiteRoom();
 
-    System.out.println("Welcome to the Hotel Booking System");
-    System.out.println("Application Name: Hotel Reservation Manager");
-    System.out.println("Version: 1.0");
+    int singleAvailability = 10;
+    int doubleAvailability = 5;
+    int suiteAvailability = 2;
 
-
+    System.out.println("=== Hotel Room Availability ===");
+    single.displayDetails(singleAvailability);
+    cpuDouble.displayDetails(doubleAvailability);
+    suite.displayDetails(suiteAvailability);
   }
 }
